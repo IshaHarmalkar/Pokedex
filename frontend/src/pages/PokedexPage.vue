7b79dfc42d91fbd47d6a83a6f4eb6d04e7c9cfda
IshaHarmalkar <167905999+IshaHarmalkar@users.noreply.github.com>
2025-07-25 23:05:17 +0530

    Cut down on loading time by prefetching. Worked for main pokedex grid pages. Does not work for details page, since images are remotely hosted

--- frontend/src/pages/PokedexPage.vue
+++ frontend/src/pages/PokedexPage.vue
@@ -1,17 +1,22 @@
 <template>
   <q-page class="q-pa-md q-mt-md">
+    <!-- Pokemon Grid -->
     <div class="row q-col-gutter-md">
-      <div
-        v-for="pokemon in pokemons"
-        :key="pokemon.id"
-        class="col-xs-12 col-sm-6 col-md-4 col-lg-3"
-      >
-        <PokemonCard :pokemon="pokemon" @card-clicked="goToDetails" />
-      </div>
-    </div>
+      <template v-if="loading">
+        <div v-for="n in 20" :key="'skeleton-' + n" class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+          <q-skeleton height="220px" square class="q-mb-sm" />
+        </div>
+      </template>
 
-    <div v-if="loading" class="text-center q-mt-md">
-      <q-spinner color="primary" size="40px" />
+      <template v-else>
+        <div
+          v-for="pokemon in pokemons"
+          :key="pokemon.id"
+          class="col-xs-12 col-sm-6 col-md-4 col-lg-3"
+        >
+          <PokemonCard :pokemon="pokemon" @card-clicked="goToDetails" />
+        </div>
+      </template>
     </div>
 
     <div v-if="error" class="text-negative text-center q-mt-md">
@@ -23,16 +28,11 @@
         label="Previous"
         color="primary"
         :disable="!prevPageUrl"
-        @click="fetchPokemons(currentPage - 1)"
+        @click="goToUrl(prevPageUrl)"
         class="q-mr-sm"
       />
 
-      <q-btn
-        label="Next"
-        color="primary"
-        :disable="!nextPageUrl"
-        @click="fetchPokemons(currentPage + 1)"
-      />
+      <q-btn label="Next" color="primary" :disable="!nextPageUrl" @click="goToUrl(nextPageUrl)" />
     </div>
   </q-page>
 </template>
@@ -49,12 +49,14 @@ export default {
   data() {
     return {
       pokemons: [],
-      loading: true,
+      cachedNextPage: null,
+      loading: false,
       error: null,
       currentPage: 1,
       lastPage: null,
       nextPageUrl: null,
       prevPageUrl: null,
+      paginationMeta: {},
     }
   },
 
@@ -71,16 +73,14 @@ export default {
     this.fetchPokemons()
   },
   methods: {
-    async fetchPokemons(page = 1) {
-      this.loading = true
+    async fetchPokemons(url = 'pokemon') {
+      // this.loading = true
       this.error = null
+
       try {
-        const res = await this.$api.get(`/pokemon?page=${page}`)
-        this.pokemons = res.data.data
-        this.currentPage = res.data.current_page
-        this.lastPage = res.data.last_page
-        this.nextPageUrl = res.data.next_page_url
-        this.prevPageUrl = res.data.prev_page_url
+        const data = await this.getPokemonPage(url)
+        this.applyPageData(data)
+        this.prefetchNextPage(data.next_page_url)
       } catch (err) {
         this.error = 'Failed to load Pokemon'
         console.log(err)
@@ -88,8 +88,55 @@ export default {
         this.loading = false
       }
     },
+
+    async getPokemonPage(url) {
+      const res = await this.$api.get(url)
+      return res.data
+    },
+
+    applyPageData(data) {
+      this.pokemons = data.data
+      this.nextPageUrl = data.next_page_url
+      this.prevPageUrl = data.prev_page_url
+      this.paginationMeta = {
+        total: data.total,
+        current: data.current_page,
+        perPage: data.per_page,
+        lastPage: data.last_page,
+      }
+    },
+
+    async prefetchNextPage(url) {
+      if (!url) {
+        this.cachedNextPage = null
+        return
+      }
+
+      try {
+        const res = await this.$api.get(url)
+        this.cachedNextPage = res.data
+      } catch {
+        this.cachedNextPage = null
+      }
+    },
+
+    async goToUrl(url) {
+      if (url === this.nextPageUrl && this.cachedNextPage) {
+        this.applyPageData(this.cachedNextPage)
+        this.prefetchNextPage(this.cachedNextPage.next_page_url)
+        this.cachedNextPage = null
+      } else {
+        await this.fetchPokemons(url)
+      }
+    },
+
     goToDetails(pokemon) {
       const identifier = pokemon.name?.toLowerCase() || pokemon.pokedex_number || pokemon.id
+      const imageUrl = pokemon.official_artwork_url || pokemon.sprite_url
+      if (imageUrl) {
+        const preloadImg = new Image()
+        preloadImg.src = imageUrl
+      }
       this.$router.push({
         name: 'pokemon-details',
         params: {
